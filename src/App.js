9342db530d987994dc643e67a32f8490f68aed2c
Valeed Anjum <allinone644@gmail.com>
2019-05-24 12:35:00 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Navbar from './compnents/Navbar/Navbar';
+import Tourlist from './compnents/Tourlist/Tourlist';
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Tourlist />
+    </main>
+  );
+}
+
+export default App;
